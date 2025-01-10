121f05d2229278138197f248cdb52c82278eb233
Michal <michalhavlik2@gmail.com>
2025-01-10 18:04:17 +0100

    footer

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,18 +1,28 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col } from 'react-bootstrap';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear()
-
+  const currentYear = new Date().getFullYear();
+  const github = process.env.GITHUB;
   return (
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">
+          <Col className='text-center py-3'>
+            <a
+              href='https://github.com/Technohunter96/BookStore'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='github-icon cursor pointer'
+            >
+              <FaGithub size={24} />
+            </a>
             <p>BookStore &copy; {currentYear}</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
-export default Footer
+  );
+};
+
+export default Footer;
